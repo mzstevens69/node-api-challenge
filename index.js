b61f33cef19c44e4994110de52d3ab8a69a5d75b
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2020-01-17 10:48:54 -0600

    added api-server.js, router-actionrouter.js,projectrouter, index.js, questions answered

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const server = require("./api/server");
+
+const port = process.env.PORT || 6000;
+
+server.listen(port, () => {
+    console.log(`\n!!! Server rocking http://localhost${port} !!!\n`);
+});
